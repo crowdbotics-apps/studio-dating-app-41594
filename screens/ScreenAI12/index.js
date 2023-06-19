007c19d99b01bb1b0e94b5730e0b00d4f21c5199
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-06-19 18:52:11 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI12/index.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+
+const RelationshipScreen = () => {
+  const [selectedRelationships, setSelectedRelationships] = useState([]);
+  const relationships = [{
+    id: '1',
+    label: 'Friendship'
+  }, {
+    id: '2',
+    label: 'Long-term Relationship'
+  }, {
+    id: '3',
+    label: 'Casual Dating'
+  }, {
+    id: '4',
+    label: 'Marriage'
+  }];
+
+  const toggleRelationship = id => {
+    if (selectedRelationships.includes(id)) {
+      setSelectedRelationships(selectedRelationships.filter(item => item !== id));
+    } else {
+      setSelectedRelationships([...selectedRelationships, id]);
+    }
+  };
+
+  const renderItem = ({
+    item
+  }) => <TouchableOpacity style={[styles.checkbox, selectedRelationships.includes(item.id) && styles.selected]} onPress={() => toggleRelationship(item.id)}>
+      <Text style={styles.label}>{item.label}</Text>
+    </TouchableOpacity>;
+
+  return <View style={styles.container}>
+      <Text style={styles.title}>Select Relationship Types</Text>
+      <FlatList data={relationships} renderItem={renderItem} keyExtractor={item => item.id} extraData={selectedRelationships} />
+      <TouchableOpacity style={styles.nextButton} onPress={() => console.log(selectedRelationships)}>
+        <Text style={styles.nextButtonText}>Next</Text>
+      </TouchableOpacity>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  checkbox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  },
+  selected: {
+    backgroundColor: '#f0f0f0'
+  },
+  label: {
+    marginLeft: 10
+  },
+  nextButton: {
+    backgroundColor: '#007AFF',
+    borderRadius: 5,
+    padding: 10,
+    paddingHorizontal: 20,
+    marginTop: 20
+  },
+  nextButtonText: {
+    color: '#fff',
+    fontWeight: 'bold'
+  }
+});
+export default RelationshipScreen;
